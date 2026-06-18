1b0242c12441b2e9ebcfff766e586c5ab23a56f1
pranjalr004 <pranjalr376@gmail.com>
2026-06-18 09:52:54 +0530

    Refactor SQL query for engagement_data table

--- Queries/Engagement_Data.sql
+++ Queries/Engagement_Data.sql
@@ -1,17 +1,17 @@
 -- Query to clean and normalize the engagement_data table
 
 SELECT 
-    EngagementID,  -- Selects the unique identifier for each engagement record
-    ContentID,  -- Selects the unique identifier for each piece of content
-	CampaignID,  -- Selects the unique identifier for each marketing campaign
+    EngagementID, 
+    ContentID,  
+	CampaignID,  
     ProductID,  -- Selects the unique identifier for each product
-    UPPER(REPLACE(ContentType, 'Socialmedia', 'Social Media')) AS ContentType,  -- Replaces "Socialmedia" with "Social Media" and then converts all ContentType values to uppercase
-    LEFT(ViewsClicksCombined, CHARINDEX('-', ViewsClicksCombined) - 1) AS Views,  -- Extracts the Views part from the ViewsClicksCombined column by taking the substring before the '-' character
-    RIGHT(ViewsClicksCombined, LEN(ViewsClicksCombined) - CHARINDEX('-', ViewsClicksCombined)) AS Clicks,  -- Extracts the Clicks part from the ViewsClicksCombined column by taking the substring after the '-' character
+    UPPER(REPLACE(ContentType, 'Socialmedia', 'Social Media')) AS ContentType,  
+    LEFT(ViewsClicksCombined, CHARINDEX('-', ViewsClicksCombined) - 1) AS Views,  
+    RIGHT(ViewsClicksCombined, LEN(ViewsClicksCombined) - CHARINDEX('-', ViewsClicksCombined)) AS Clicks,
     Likes,  -- Selects the number of likes the content received
     -- Converts the EngagementDate to the dd.mm.yyyy format
-    FORMAT(CONVERT(DATE, EngagementDate), 'dd.MM.yyyy') AS EngagementDate  -- Converts and formats the date as dd.mm.yyyy
+    FORMAT(CONVERT(DATE, EngagementDate), 'dd.MM.yyyy') AS EngagementDate
 FROM 
-    dbo.engagement_data  -- Specifies the source table from which to select the data
+    dbo.engagement_data  
 WHERE 
-    ContentType != 'Newsletter';  -- Filters out rows where ContentType is 'Newsletter' as these are not relevant for our analysis
+    ContentType != 'Newsletter';
